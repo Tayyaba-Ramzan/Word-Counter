11e0c3e40177345d4bce7e2a1ac0912a1d93517c
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-05-25 22:54:24 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers: {
+    Sentence: string;
+} = await inquirer.prompt([
+    {
+        message: "Enter a Sentence to count the word:",
+        type: "input",
+        name: "Sentence"
+    }
+
+])
+const words = answers.Sentence.trim().split(" ");
+console.log(chalk.green(words));
+console.log(chalk.yellow(`Your Sentence word count is ${words.length}`));
+
+//                         XXXXXXXXXXXXXXXXXXXXXXXXX
